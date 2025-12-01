ed0c4a945b37ef5f2248123f1ec119a957a4e7a6
EERON36 <aaron.sofonias@outlook.com>
2025-12-01 22:45:01 +0100

    started day1 part 2/2

--- aoe25/day1/01.py
+++ aoe25/day1/01.py
@@ -2,6 +2,10 @@ def rdial(turndial, code):
     total = turndial + code
     while total > 99:
         total = total - 100
+        global counter
+        counter = counter + 1
+        # print("global +1")
+        # print(counter)
     return total
 
 
@@ -9,25 +13,33 @@ def ldial(turndial, code):
     total = turndial - code
     while total < 0:
         total = total + 100
+        global counter
+        counter = counter + 1
+        # print("global +1")
+        # print(counter)
     return total
 
 
 def main():
     position = 50
+    global counter
     counter = 0
 
     with open("dial.txt", "r") as code:
         for line in code:
-            print(line)
-            print(position)
+            line = line.strip()
+            # print(line)
+            # print(position)
             if line[0] == "R":
                 position = rdial(position, int(line[1:]))
 
             else:
                 position = ldial(position, int(line[1:]))
 
-            if position == 0:
-                counter += 1
+            # if position == 0:
+            #     print("+1 LAST COUNTER LINE")
+            #     counter += 1
+            #     print(counter)
 
         print(counter)
 
