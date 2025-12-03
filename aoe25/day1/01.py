09b30181056e006ad0e3782c7ec1e6589922d075
EERON36 <aaron.sofonias@outlook.com>
2025-12-03 19:54:59 +0100

    DAY 1 FINISHED

--- aoe25/day1/01.py
+++ aoe25/day1/01.py
@@ -2,22 +2,50 @@ def main():
     position = 50
     global counter
     counter = 0
+    # laps = 0
 
     with open("dial.txt", "r") as code:
         for line in code:
             line = line.strip()
 
-        if int(line[1:] >= 100):
-            # 100 = 1 varv
-            # räkna hur nånga
-            # mod 100 för rest värdet
+            direction = line[0]
+            code = int(line[1:])
 
-            # left turn
-            # samam sak som right,
-            """Pos 25 -> L50
-            25 -> 75
-            100-25 = 50 -> R50
-            25+50 = 75"""
+            # coiunt laps
+            if code >= 100:
+                counter = counter + code // 100
+                # counter = counter + laps
+                print(f" LLAPS {laps} AND CODE {code}")
+
+            # figure out enw position
+            remainder = code % 100
+
+            if direction == "R":
+                new_position = position + remainder
+
+                if new_position > 100:
+                    counter = counter + 1
+
+                position = new_position % 100
+
+            else:  # LX -> R(100-X)
+
+                if position is not 0:
+
+                    if remainder > position:
+                        counter = counter + 1
+
+                new_position = position + (100 - remainder)
+                position = new_position % 100
+
+            if position == 0:
+                counter = counter + 1
+
+            # counter = counter + laps
+
+            print(f"Code {line}")
+            print(f"Position {position}")
+            print(f"Laps {laps} Counter {counter}")
 
 
 # Run the program
