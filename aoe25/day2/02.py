431bca7be579742b2507c88cecfd8eaa85f477a1
EERON36 <aaron.sofonias@outlook.com>
2025-12-06 15:00:53 +0100

    struggleron

--- aoe25/day2/02.py
+++ aoe25/day2/02.py
@@ -1,8 +1,21 @@
 with open("test.txt", "r") as file:
+    # Loop 1
     for line in file:
         id_list = line.strip().split(",")
-        print(id_list)
+        # print(id_list)
 
+        # loop 2
         for group in id_list:
-            print("Group")
-            print(group)
+            # skip last symbol
+            if not group:
+                continue
+            # print("Group")
+            # print(group)
+            parts = group.split("-")
+            # print("parts")
+            # print(parts)
+            first_part = int(parts[0])
+            second_part = int(parts[1])
+            print(f"First_part = {first_part} Secon part = {second_part}")
+
+            # Loops 3?
