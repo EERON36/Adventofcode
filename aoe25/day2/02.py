3daff8c4481448213a59805fbd632c22fb5cf9da
EERON36 <aaron.sofonias@outlook.com>
2025-12-07 00:42:49 +0100

    Day 2 1/2 DONE

--- aoe25/day2/02.py
+++ aoe25/day2/02.py
@@ -1,4 +1,6 @@
-with open("test.txt", "r") as file:
+invalid_id = 0
+
+with open("input.txt", "r") as file:
     # Loop 1
     for line in file:
         id_list = line.strip().split(",")
@@ -16,6 +18,30 @@ with open("test.txt", "r") as file:
             # print(parts)
             first_part = int(parts[0])
             second_part = int(parts[1])
-            print(f"First_part = {first_part} Secon part = {second_part}")
+            # print(f"First_part = {first_part} Secon part = {second_part}")
 
-            # Loops 3?
+            # loop 3
+            # get lenght of string
+            # find highest possible seq ex. 1010 (2) 10 10  then (1)
+            # add to ivnalid ids
+            # 666666
+            # 3
+            # 012  345
+            for s in range(first_part, second_part + 1):
+                lenght_s = len(str(s))
+                s_str = str(s)
+                lenght_pairs = int(lenght_s / 2)
+
+                if lenght_s % 2 == 0:
+                    # index till split
+                    first = s_str[0:lenght_pairs]
+                    # print(first)
+                    # index split + resten
+                    second = s_str[lenght_pairs:]
+                    # print(second)
+                    if first == second:
+                        invalid_id = invalid_id + s
+                    else:
+                        continue
+
+print(invalid_id)
